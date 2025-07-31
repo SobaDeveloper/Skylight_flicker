08a8e0a3e8e71a41f417d5ccfcf8f3e27058bf4a
Levi Hsiao <levi@glorify-app.com>
2025-07-31 14:06:01 -0700

    Implemented photo screens navigation

---
+++ app/src/main/java/com/example/skylightflickr/feature/main/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.skylightflickr.feature.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import com.example.skylightflickr.navigation.FlickrNavigation
+import com.example.skylightflickr.ui.theme.SkylightFlickrTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SkylightFlickrTheme {
+                FlickrNavigation()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/skylightflickr/navigation/PhotoNavigation.kt
@@ -0,0 +1,57 @@
+package com.example.skylightflickr.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.skylightflickr.feature.photodetails.PhotoDetailsScreen
+import com.example.skylightflickr.feature.photosearch.PhotoSearchScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+object SearchScreen
+
+@Serializable
+data class PhotoDetailScreen(
+    val photoId: String,
+    val secret: String = ""
+)
+
+@Composable
+fun FlickrNavigation(
+    navController: NavHostController = rememberNavController(),
+) {
+    NavHost(
+        navController = navController,
+        startDestination = SearchScreen
+    ) {
+        composable<SearchScreen> {
+            PhotoSearchScreen(
+                onPhotoClick = { photo ->
+                    navController.navigate(
+                        PhotoDetailScreen(
+                            photoId = photo.id,
+                            secret = photo.secret
+                        )
+                    )
+                }
+            )
+        }
+
+        composable<PhotoDetailScreen> { backStackEntry ->
+            val photoDetail = backStackEntry.arguments?.let {
+                PhotoDetailScreen(
+                    photoId = it.getString("photoId") ?: "",
+                    secret = it.getString("secret") ?: ""
+                )
+            } ?: return@composable
+
+            PhotoDetailsScreen(
+                photoId = photoDetail.photoId,
+                secret = photoDetail.secret,
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+    }
+}
